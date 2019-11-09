f331b88c96f59cc3cf3ac915a0adcdbc880b3b34
Samplehoff <sampey_dchi@yahoo.com>
2019-11-09 09:35:06 -0600

    new

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     function renderBrewery(breweryArray) {
         var breweryHTML = breweryArray.map(function(brewery) {
@@ -10,14 +16,33 @@ document.addEventListener('DOMContentLoaded', function() {
                     <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title">${brewery.name}</h5>
-                            <p class="card-text"></p>
-                            <a href="#" class="btn btn-primary">Address</a>
+                            <p class="card-text">${brewery.opening_hours}</p>
+                            <a href="${brewery.formatted_address}" class="btn btn-primary">Go to Address</a>
                         </div>
                     </div>
                 </div>
             </div>`
         })
+        return breweryHTML.join('');
     }
+    $(document).ready(function() {
+        $('#brewey-btn').click(getBrewery)
+    });
+    function getBrewery() {
+        $.getJSON('https://maps.googleapis.com/maps/api/place/textsearch/json?query=breweries+in+Houston&key=AIzaSyBitP27AVOxIuXMYSh1w8YAxKcpVYk2_Pg'), function(response) {
+            $('#search-container').attr(response.message)
+            console.log(response)
+        }
+    }
+    const searchBtn = document.getElementById("search-btn");
+    var content = document.getElementById('search-container');
+    const breweryBtn = document.getElementById('brewery-btn');
+    breweryBtn.addEventListener('submit', function(e) {
+        e.preventDefault()
+        console.log(e)
+    })
+    console.log(getBrewery())
+    
 })
 
 
@@ -25,17 +50,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-`<div class="card mb-3" style="max-width: 540px;">
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="renderPhoto()" class="card-img" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title"></h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Address</a>
-      </div>
-    </div>
-  </div>
-</div>`
+
+
