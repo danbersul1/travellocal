ff45cd5b24ca66100598a2edebc122a157b9fe3d
Samplehoff <sampey_dchi@yahoo.com>
2019-11-09 11:16:52 -0600

    new

--- index.js
+++ index.js
@@ -1,54 +1,66 @@
+$(document).ready(function () {
 
+    var placeData = [];
 
+    function renderAtt(arr) {
 
+        var render = arr.map(function (data) {
+            var photos = '';
+            if (typeof data.photos != 'undefined') {
+                photos = data.photos.map(function (photo) {
+                    console.log(photo)
+                    return `<img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo.photo_reference}&key=AIzaSyBitP27AVOxIuXMYSh1w8YAxKcpVYk2_Pg" class="card-img" alt="..."/>`
+                });
+            }
 
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    function renderBrewery(breweryArray) {
-        var breweryHTML = breweryArray.map(function(brewery) {
-            return 
-            `<div class="card mb-3" style="max-width: 540px;">
-                <div class="row no-gutters">
-                    <div class="col-md-4">
-                        <img src="renderPhoto()" class="card-img" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">${brewery.name}</h5>
-                            <p class="card-text">${brewery.opening_hours}</p>
-                            <a href="${brewery.formatted_address}" class="btn btn-primary">Go to Address</a>
+            var card = `<div class="card mb-3" style="max-width: 540px;">
+                        <div class="row no-gutters">
+                            <div class="col-md-4">
+                            ${photos}
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">${data.name}</h5>
+                                    <p class="card-text">${data.formatted_address}</p>
+                                    <p class="card-text">Rating ${data.rating}</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>`
+                    </div>`
+
+            return card;
+
         })
-        return breweryHTML.join('');
+        return render;
     }
-    $(document).ready(function() {
-        $('#brewey-btn').click(getBrewery)
-    });
-    function getBrewery() {
-        $.getJSON('https://maps.googleapis.com/maps/api/place/textsearch/json?query=breweries+in+Houston&key=AIzaSyBitP27AVOxIuXMYSh1w8YAxKcpVYk2_Pg'), function(response) {
-            $('#search-container').attr(response.message)
-            console.log(response)
-        }
-    }
-    const searchBtn = document.getElementById("search-btn");
-    var content = document.getElementById('search-container');
-    const breweryBtn = document.getElementById('brewery-btn');
-    breweryBtn.addEventListener('submit', function(e) {
-        e.preventDefault()
-        console.log(e)
+
+
+
+    $("#brewery-btn").click(function () {
+        event.preventDefault();
+        console.log("test");
+
+        axios.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=breweries+in+Houston&key=AIzaSyBitP27AVOxIuXMYSh1w8YAxKcpVYk2_Pg")
+            .then(function (respond) {
+                placeData = respond.data.results
+                console.log(placeData);
+                var refernce = placeData.map(function (arr) {
+                    var photoData = arr.photos
+
+
+                    return photoData;
+                })
+
+                console.log(refernce);
+
+
+
+                $("#search-container").html(renderAtt(placeData))
+
+                return refernce;
+            })
+
+
     })
-    console.log(getBrewery())
-    
 })
-
-
-
-
-
-
-
-
