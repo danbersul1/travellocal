b361286d16bfc79dfbe82d9b515410d403d943e9
mohammed mustafa <mohammed.alkhddar@gmail.com>
2019-11-12 18:18:16 -0600

    update

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
             <p class="card-text">Rating ${data.rating}</p>
           </div>
         </div><iframe width="600" height="450" frameborder="0" style="border:0"
-        src="https://www.google.com/maps/embed/v1/place?q=place_id:${data.place_id}&key=AIzaSyBJDqK23uOVafUHXPDZzEpQ7i4XiRWvwW8" allowfullscreen></iframe>
+        src="https://www.google.com/maps/embed/v1/place?q=place_id:${data.place_id}&key=AIzaSyBJDqK23uOVafUHXPDZzEpQ7i4XiRWvwW8" allowfullscreen></iframe> 
 
       </div>
       
@@ -183,6 +183,7 @@ return photoData;
 
 
 })
+
 
   
   
